06776394110916f8bad28f1ffdce5db846eedd63
Paramastri Ardiningrum <37019687+paramastri@users.noreply.github.com>
2020-04-14 12:50:14 +0700

    new project

---
+++ apps/modules/dashboard/Presentation/Web/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Its\Example\Dashboard\Presentation\Web\Controller;
+
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        return "<h1>Homepages!</h1>";
+    }
+
+    public function homeAction()
+    {
+        $this->view->pick('home');
+    }
+}
